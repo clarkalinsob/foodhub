dbc1b0544d51d96985f89a2533a84a4f2f0cd049
unknown <clarkalinsob101@gmail.com>
2020-05-28 17:45:57 +0800

    fix signup errors

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -28,7 +28,7 @@ function Signup(props) {
       props.history.push('/')
     },
     onError(err) {
-      setErrors(err.graphQLErrors[0].extensions.exception.errors)
+      if (err.graphQLErrors.length > 0) setErrors(err.graphQLErrors[0].extensions.exception.errors)
     }
   })
 
@@ -50,7 +50,7 @@ function Signup(props) {
       props.history.push('/')
     },
     onError(err) {
-      setErrors(err.graphQLErrors[0].extensions.exception.errors)
+      if (err.graphQLErrors.length > 0) setErrors(err.graphQLErrors[0].extensions.exception.errors)
     }
   })
 
@@ -64,63 +64,63 @@ function Signup(props) {
   // END Sign up with Google
 
   return (
-    <div className='form-container'>
+    <div className="form-container">
       <Segment>
-        <div className='segment'>
+        <div className="segment">
           <Form onSubmit={onSubmit} noValidate className={loading ? 'loading' : ''}>
             <h1>Create a Foodhub Account</h1>
             <h3>
-              <a href='/signin'>
+              <a href="/signin">
                 <u> or sign in to your account</u>
               </a>
             </h3>
             <br />
             <Form.Input
-              label='Given Name'
-              placeholder='e.g., Clark, Egbert'
-              name='givenName'
-              type='text'
+              label="Given Name"
+              placeholder="e.g., Clark, Egbert"
+              name="givenName"
+              type="text"
               value={values.givenName}
               error={errors.givenName ? true : false}
               onChange={onChange}
             />
             <Form.Input
-              label='Family Name'
-              placeholder='e.g., Alinsob, Tagalog'
-              name='familyName'
-              type='text'
+              label="Family Name"
+              placeholder="e.g., Alinsob, Tagalog"
+              name="familyName"
+              type="text"
               value={values.familyName}
               error={errors.familyName ? true : false}
               onChange={onChange}
             />
             <Form.Input
-              label='Email Address'
-              placeholder='e.g., clark.alinsob@email.com'
-              name='email'
-              type='email'
+              label="Email Address"
+              placeholder="e.g., clark.alinsob@email.com"
+              name="email"
+              type="email"
               value={values.email}
               error={errors.email ? true : false}
               onChange={onChange}
             />
             <Form.Input
-              label='Password'
-              placeholder='e.g., &#183;&#183;&#183;&#183;&#183;&#183;&#183;'
-              name='password'
-              type='password'
+              label="Password"
+              placeholder="e.g., &#183;&#183;&#183;&#183;&#183;&#183;&#183;"
+              name="password"
+              type="password"
               value={values.password}
               error={errors.password ? true : false}
               onChange={onChange}
             />
             <Form.Input
-              label='Confirm Password'
-              placeholder='e.g., &#183;&#183;&#183;&#183;&#183;&#183;&#183;'
-              name='confirmPassword'
-              type='password'
+              label="Confirm Password"
+              placeholder="e.g., &#183;&#183;&#183;&#183;&#183;&#183;&#183;"
+              name="confirmPassword"
+              type="password"
               value={values.confirmPassword}
               error={errors.confirmPassword ? true : false}
               onChange={onChange}
             />
-            <Button fluid type='submit' positive>
+            <Button fluid type="submit" positive>
               Register
             </Button>
           </Form>
@@ -129,21 +129,21 @@ function Signup(props) {
             clientId={GOOGLE_CLIENT_ID}
             render={renderProps => (
               <Button fluid primary onClick={renderProps.onClick} disabled={renderProps.disabled}>
-                <Icon name='google' /> Sign up with Google
+                <Icon name="google" /> Sign up with Google
               </Button>
             )}
-            buttonText='Sign up with Google'
+            buttonText="Sign up with Google"
             onSuccess={responseGoogle}
             onFailure={responseGoogle}
             cookiePolicy={'single_host_origin'}
           />
           {Object.keys(errors).length > 0 && (
-            <div className='ui error message'>
-              <div className='content'>
-                <div className='header'>There was some errors with your submission</div>
-                <ul className='list'>
+            <div className="ui error message">
+              <div className="content">
+                <div className="header">There was some errors with your submission</div>
+                <ul className="list">
                   {Object.values(errors).map(value => (
-                    <li className='content' key={value}>
+                    <li className="content" key={value}>
                       {value}
                     </li>
                   ))}
